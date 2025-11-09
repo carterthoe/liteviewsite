77b1f08c62c5d831c4db4efcd3953862726757b6
carterthoe <ctcy5@umsystem.edu>
2025-11-09 09:22:54 -0600

    Change vite back

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,13 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://liteview.216.128.151.36.sslip.io',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+    plugins: [react()],
 })
