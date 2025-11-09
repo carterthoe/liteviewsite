1b83327c29cfbbe8810f4ea2de2b1b77b3ddb66d
carterthoe <ctcy5@umsystem.edu>
2025-11-09 04:26:34 -0600

    fix issues

--- Frontend/src/app/main.jsx
+++ Frontend/src/app/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { Auth0Provider } from '@auth0/auth0-react'
-import App from './app'
+import App from './App'
 import '../styles/index.css'
 
 createRoot(document.getElementById('root')).render(
